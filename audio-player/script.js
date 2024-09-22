e1ecc0f0ba3b781bc9ded43fb260eef782e5ceeb
Pavel Gordienko <guzmo@yandex.ru>
2024-09-23 02:14:17 +0300

    feat: add play pause favicon

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const audio = new Audio(
+    "./assets/music/1.mp3"
+);
+
+// console.dir(audio);
+
+const playPauseIcon = document.getElementById("play-pause-icon");
+
+playPauseIcon.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+        playPauseIcon.classList.remove("toggle-play");
+        playPauseIcon.classList.add("toggle-pause");
+    } else {
+        audio.pause();
+        playPauseIcon.classList.remove("toggle-pause");
+        playPauseIcon.classList.add("toggle-play");
+    }
+});
+
